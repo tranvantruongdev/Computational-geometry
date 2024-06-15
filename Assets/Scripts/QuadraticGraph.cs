266ac09eebb1b51e2e360dd8255ec659fec9353f
tranvantruongdev <tranvantruong.dev@gmail.com>
2024-06-15 20:56:26 +0700

    Add UI QuadraticGraph

--- Assets/Scripts/QuadraticGraph.cs
+++ Assets/Scripts/QuadraticGraph.cs
@@ -1,19 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class QuadraticGraph : MonoBehaviour
 {
     [SerializeField]
     private LineRenderer lineRenderer;
 
-    [SerializeField]
-    private int numPoints = 100;  // Number of points to plot
+    private readonly int numPoints = 100;  // Number of points to plot
+
+    private readonly float xMin = -10f;  // Minimum x value
+    private readonly float xMax = 10f;   // Maximum x value
 
     [SerializeField]
-    private float xMin = -10f;  // Minimum x value
+    private TMPro.TMP_InputField ifA;
     [SerializeField]
-    private float xMax = 10f;   // Maximum x value
+    private TMPro.TMP_InputField ifB;
+    [SerializeField]
+    private TMPro.TMP_InputField ifC;
+    [SerializeField]
+    private Button btnDraw;
 
-    public void DrawQuadraticGraph(float a, float b, float c)
+    private void Awake()
+    {
+        btnDraw.onClick.AddListener(DrawQuadraticGraph);
+    }
+
+    public void DrawQuadraticGraph()
     {
         if (lineRenderer == null)
         {
@@ -21,18 +33,31 @@ public class QuadraticGraph : MonoBehaviour
             return;
         }
 
-        lineRenderer.positionCount = numPoints;
+        float a = float.Parse(ifA.text.Trim());
+        float b = float.Parse(ifB.text.Trim());
+        float c = float.Parse(ifC.text.Trim());
+
+        float yLimit = 10f;
+
         Vector3[] positions = new Vector3[numPoints];
         float step = (xMax - xMin) / (numPoints - 1);
+        int validPoints = 0;
 
         for (int i = 0; i < numPoints; i++)
         {
-            float x = xMin + i * step;
-            float y = a * x * x + b * x + c;
-            positions[i] = new Vector3(x, y, 0);
+            float x = xMin + (i * step);
+            float y = (a * x * x) + (b * x) + c;
+
+            if (Mathf.Abs(y) > yLimit)
+            {
+                break;
+            }
+
+            positions[validPoints] = new Vector3(x, y, 0);
+            validPoints++;
         }
 
+        lineRenderer.positionCount = validPoints;
         lineRenderer.SetPositions(positions);
     }
 }
-
