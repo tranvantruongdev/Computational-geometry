13b565df3caeb79ae2aa5a46f94de6bbc99d7266
Echo Of Steel <thanhanforwork@gmail.com>
2024-06-17 21:28:58 +0700

    Update SpriteRendererGraph.cs

--- Assets/Graph/SpriteRendererGraph.cs
+++ Assets/Graph/SpriteRendererGraph.cs
@@ -10,13 +10,15 @@ public class SpriteRendererGraph : MonoBehaviour
     
     private Camera _mainCamera;
     private Vector3 _lastPosition;
+    private float _size;
 
+    [Header("__SETTINGS__")]
+    public Vector2Int line = new Vector2Int(7, 7);
+    public Vector2 space;
+    public float lineThickness = 1f;
+    [Space]
     [SerializeField]
     private SpriteRenderer _spritePrefab;
-    [Space]
-    [Header("__SETTINGS__")]
-    [SerializeField]    
-    private Vector2Int _line = new Vector2Int(7, 7);
     [Space]
     [Header("__COLOR__")]
     [SerializeField]
@@ -31,7 +33,7 @@ public class SpriteRendererGraph : MonoBehaviour
     {
         _mainCamera = Camera.main;
         _lastPosition = _mainCamera.transform.position;
-        CreateLines(_line);
+        CreateLines(line);
     }
 
     private void Update()
@@ -39,39 +41,50 @@ public class SpriteRendererGraph : MonoBehaviour
         if (_mainCamera.transform.position.x > _lastPosition.x)
         {
             Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
-            OnCameraMoveRight(max);
+            Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
+            OnCameraMoveRight(max, min);
         }
         else if (_mainCamera.transform.position.x < _lastPosition.x)
         {
+            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
             Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
-            OnCameraMoveLeft(min);
+            OnCameraMoveLeft(max, min);
         }
 
-        if (Camera.main.transform.position.y > _lastPosition.y)
+        if (_mainCamera.transform.position.y > _lastPosition.y)
         {
-            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(Camera.main, transform);
-            OnCameraMoveUp(max);
+            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
+            Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
+            OnCameraMoveUp(max, min);
         }
-        else if (Camera.main.transform.position.y < _lastPosition.y)
+        else if (_mainCamera.transform.position.y < _lastPosition.y)
         {
-            Vector3 min = CameraHelper.GetMinViewFromCameraTo(Camera.main, transform);
-            OnCameraMoveDown(min);
+            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
+            Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
+            OnCameraMoveDown(max, min);
         }
 
-        if (Camera.main.transform.position.z < _lastPosition.z)
+        bool isPerspectiveZoomOut = !_mainCamera.orthographic && _mainCamera.transform.position.z < _lastPosition.z;
+        bool isPerspectiveZoomIn = !_mainCamera.orthographic && _mainCamera.transform.position.z > _lastPosition.z;
+
+        bool isOrthographicZoomOut = _mainCamera.orthographic && _mainCamera.orthographicSize > _size;
+        bool isOrthographicZoomIn = _mainCamera.orthographic && _mainCamera.orthographicSize < _size;
+
+        if (isPerspectiveZoomOut || isOrthographicZoomOut)
         {
-            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(Camera.main, transform);
-            Vector3 min = CameraHelper.GetMinViewFromCameraTo(Camera.main, transform);
+            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
+            Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
             OnCameraZoomOut(max, min);
         }
-        else if (Camera.main.transform.position.z > _lastPosition.z)
+        else if (isPerspectiveZoomIn || isOrthographicZoomIn)
         {
-            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(Camera.main, transform);
-            Vector3 min = CameraHelper.GetMinViewFromCameraTo(Camera.main, transform);
+            Vector3 max = CameraHelper.GetMaxViewFromCameraTo(_mainCamera, transform);
+            Vector3 min = CameraHelper.GetMinViewFromCameraTo(_mainCamera, transform);
             OnCameraZoomIn(max, min);
         }
 
         _lastPosition = _mainCamera.transform.position;
+        _size = _mainCamera.orthographicSize;
     }
 
     private void OnDrawGizmos()
@@ -88,7 +101,7 @@ public class SpriteRendererGraph : MonoBehaviour
         for (int x = (int)(-expand.x / 2); x < expand.x / 2; x++)
         {
             SpriteRenderer sprite = GetLineX();
-            sprite.transform.position = new Vector3(x, 0);
+            sprite.transform.position = new Vector3(x * space.x, 0);
             AutoSetLineColorX(sprite);
             _lineX.Add(sprite);
         }
@@ -96,7 +109,7 @@ public class SpriteRendererGraph : MonoBehaviour
         for (int y = (int)(-expand.y / 2); y < expand.y / 2; y++)
         {
             SpriteRenderer sprite = GetLineFromPool(_linePoolY);
-            sprite.transform.position = new Vector3(0, y);
+            sprite.transform.position = new Vector3(0, y * space.y);
             AutoSetLineColorY(sprite);
             _lineY.Add(sprite);
         }
@@ -110,54 +123,80 @@ public class SpriteRendererGraph : MonoBehaviour
             sprite.gameObject.SetActive(true);
             return sprite;
         }
-        return Instantiate(_spritePrefab, transform);
+        SpriteRenderer newSprite = Instantiate(_spritePrefab, transform);
+        newSprite.transform.localScale = new Vector3(newSprite.transform.localScale.x, lineThickness, newSprite.transform.localScale.z);
+        return newSprite;
     }
 
-    private void OnCameraMoveRight(Vector3 max)
+    private void OnCameraMoveRight(Vector3 max, Vector3 min)
     {
         SpriteRenderer largestLine = GetLargestLineX(_lineX);
         if (largestLine.transform.position.x <= max.x)
         {
             SpriteRenderer newLine = GetLineX();
-            newLine.transform.position = largestLine.transform.position + Vector3.right;
+            newLine.transform.position = largestLine.transform.position + Vector3.right * space.x;
             AutoSetLineColorX(newLine);
             _lineX.Add(newLine);
         }
+
+        SpriteRenderer smallestLine = GetSmallestLineX(_lineX);
+        if (smallestLine.transform.position.x <= min.x)
+        {
+            PushLineXToPool(smallestLine);
+        }
     }
 
-    private void OnCameraMoveLeft(Vector3 min)
+    private void OnCameraMoveLeft(Vector3 max, Vector3 min)
     {
         SpriteRenderer smallestLine = GetSmallestLineX(_lineX);
         if (smallestLine.transform.position.x >= min.x)
         {
             SpriteRenderer newLine = GetLineX();
-            newLine.transform.position = smallestLine.transform.position + Vector3.left;
+            newLine.transform.position = smallestLine.transform.position + Vector3.left * space.x;
             AutoSetLineColorX(newLine);
             _lineX.Add(newLine);
         }
+
+        SpriteRenderer largestLine = GetLargestLineX(_lineX);
+        if (largestLine.transform.position.x >= max.x)
+        {
+            PushLineXToPool(largestLine);
+        }
     }
 
-    private void OnCameraMoveUp(Vector3 max)
+    private void OnCameraMoveUp(Vector3 max, Vector3 min)
     {
         SpriteRenderer largestLine = GetLargestLineY(_lineY);
         if (largestLine.transform.position.y <= max.y)
         {
             SpriteRenderer newLine = GetLineFromPool(_linePoolY);
-            newLine.transform.position = largestLine.transform.position + Vector3.up;
+            newLine.transform.position = largestLine.transform.position + Vector3.up * space.y;
             AutoSetLineColorY(newLine);
             _lineY.Add(newLine);
         }
+
+        SpriteRenderer smallestLine = GetSmallestLineY(_lineY);
+        if (smallestLine.transform.position.y <= min.y)
+        {
+            PushLineYToPool(smallestLine);
+        }
     }
 
-    private void OnCameraMoveDown(Vector3 min)
+    private void OnCameraMoveDown(Vector3 max, Vector3 min)
     {
         SpriteRenderer smallestLine = GetSmallestLineY(_lineY);
         if (smallestLine.transform.position.y >= min.y)
         {
             SpriteRenderer newLine = GetLineFromPool(_linePoolY);
-            newLine.transform.position = smallestLine.transform.position + Vector3.down;
+            newLine.transform.position = smallestLine.transform.position + Vector3.down * space.y;
             AutoSetLineColorY(newLine);
             _lineY.Add(newLine);
+        }
+
+        SpriteRenderer largestLine = GetLargestLineY(_lineY);
+        if (largestLine.transform.position.y >= max.y)
+        {
+            PushLineYToPool(largestLine);
         }
     }
 
@@ -168,7 +207,7 @@ public class SpriteRendererGraph : MonoBehaviour
         {
             SpriteRenderer newLine = GetLineX();
             newLine.gameObject.SetActive(true);
-            newLine.transform.position = largestLineX.transform.position + Vector3.right;
+            newLine.transform.position = largestLineX.transform.position + Vector3.right * space.x;
             AutoSetLineColorX(newLine);
             _lineX.Add(newLine);
         }
@@ -178,7 +217,7 @@ public class SpriteRendererGraph : MonoBehaviour
         {
             SpriteRenderer newLine = GetLineX();
             newLine.gameObject.SetActive(true);
-            newLine.transform.position = smallestLineX.transform.position + Vector3.left;
+            newLine.transform.position = smallestLineX.transform.position + Vector3.left * space.x;
             AutoSetLineColorX(newLine);
             _lineX.Add(newLine);
         }
@@ -188,7 +227,7 @@ public class SpriteRendererGraph : MonoBehaviour
         {
             SpriteRenderer newLine = GetLineFromPool(_linePoolY);
             newLine.gameObject.SetActive(true);
-            newLine.transform.position = largestLineY.transform.position + Vector3.up;
+            newLine.transform.position = largestLineY.transform.position + Vector3.up * space.y;
             AutoSetLineColorY(newLine);
             _lineY.Add(newLine);
         }
@@ -198,7 +237,7 @@ public class SpriteRendererGraph : MonoBehaviour
         {
             SpriteRenderer newLine = GetLineFromPool(_linePoolY);
             newLine.gameObject.SetActive(true);
-            newLine.transform.position = smallestLineY.transform.position + Vector3.down;
+            newLine.transform.position = smallestLineY.transform.position + Vector3.down * space.y;
             AutoSetLineColorY(newLine);
             _lineY.Add(newLine);
         }
